28080cad10f3f0e64afd2dc541dbe0be83f8e923
Mai Anh Vu <maianhuel@gmail.com>
2024-11-05 13:33:45 -0600

    adding dino

---
+++ data/dino.py
@@ -0,0 +1,106 @@
+from PIL import Image
+import torchvision.transforms as transforms
+import torch 
+
+class DataAugmentation():
+    """
+    """
+    def __init__(
+        self,
+        global_crops_scale=(0.4, 1),
+        local_crops_scale=(0.05, 0.4),
+        n_local_crops=8,
+        size=224,
+    ):
+        self.n_local_crops = n_local_crops
+        RandomGaussianBlur = lambda p: transforms.RandomApply(  # noqa
+            [transforms.GaussianBlur(kernel_size=5, sigma=(0.1, 2))],
+            p=p,
+        )
+
+        flip_and_jitter = transforms.Compose(
+            [
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomApply(
+                    [
+                        transforms.ColorJitter(
+                            brightness=0.4,
+                            contrast=0.4,
+                            saturation=0.2,
+                            hue=0.1,
+                        ),
+                    ]
+                ),
+                transforms.RandomGrayscale(p=0.2),
+            ]
+        )
+
+        normalize = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            ]
+        )
+
+        self.global_1 = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    size,
+                    scale=global_crops_scale,
+                    interpolation=Image.BICUBIC,
+                ),
+                flip_and_jitter,
+                RandomGaussianBlur(1.0),  # always apply
+                normalize,
+            ],
+        )
+
+        self.global_2 = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    size,
+                    scale=global_crops_scale,
+                    interpolation=Image.BICUBIC,
+                ),
+                flip_and_jitter,
+                RandomGaussianBlur(0.1),
+                transforms.RandomSolarize(170, p=0.2),
+                normalize,
+            ],
+        )
+
+        self.local = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    size,
+                    scale=local_crops_scale,
+                    interpolation=Image.BICUBIC,
+                ),
+                flip_and_jitter,
+                RandomGaussianBlur(0.5),
+                normalize,
+            ],
+        )
+
+    def __call__(self, img):
+        """Apply transformation.
+
+        Parameters
+        ----------
+        img : PIL.Image
+            Input image.
+
+        Returns
+        -------
+        all_crops : list
+            List of `torch.Tensor` representing different views of
+            the input `img`.
+        """
+        all_crops = []
+        all_crops.append(self.global_1(img))
+        all_crops.append(self.global_2(img))
+
+        all_crops.extend([self.local(img) for _ in range(self.n_local_crops)])
+
+        return all_crops
+ 
